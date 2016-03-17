7fd48968bb8ffc160af6e0645be99955fefe06f9
Davida Gaffney <gdavida@gmail.com>
2016-03-17 17:42:52 -0500

    check for lowercase working method and test working

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -1,4 +1,230 @@
+# For each rule, you'll probably want to break it out into a method. Make sure to test each method. By hand, consider writing out a flowchart, or mapping a process, or writing (by hand) some test cases.
+
+# 1 or more capital letters
+# 1 or more lower-case letters
+# 8 or more characters
+# 1 or more non-alphanumeric character
+# 1 or more numbers
+# Can't contain "password"
+# Bonus Rules to do if you get done with the previous rules and want to do more.
+
+# Can't contain the user's name
+# Can't contain sequences like "abc" or "jkl" or "456"
+
+
 require "pry"
+
+
+
+### RULE 1 -- MUST HAVE >= 1 UPCASE LETTER
+##################################
+#   Takes two strings and turns them into one string
+#
+#   first: string with the first name
+#   last: string with last name
+#
+#   returns one string
+#
+#   example:
+#   names_to_string("Davida", "Gaffney")
+# => DavidaGaffney
+# ---------------------------------
+#
+  
+# Takes a string
+# Returns true or false depending on if the string has at least one capital
+# letter
+def at_least_one_caps?(password)
+  password != password.upcase
+end
+
+
+
+
+
+
+### RULE 2 -- MUST HAVE >= 1 DOWNCASE LETTER
+##################################
+#   Takes two strings and turns them into one string
+#
+#   first: string with the first name
+#   last: string with last name
+#
+#   returns one string
+#
+#   example:
+#   names_to_string("Davida", "Gaffney")
+# => DavidaGaffney
+# ---------------------------------
+#
+  
+# Takes a string
+# Returns true or false depending on if the string has at least one capital
+# letter
+def at_least_one_downcase?(password)
+  password != password.downcase
+end
+
+
+### RULE 2 -- MUST HAVE >= 8 CHARACTERS
+##################################
+#   Takes two strings and turns them into one string
+#
+#   first: string with the first name
+#   last: string with last name
+#
+#   returns one string
+#
+#   example:
+#   names_to_string("Davida", "Gaffney")
+# => DavidaGaffney
+# ---------------------------------
+#
+
+def at_least_eight_characters?(password)
+  password.length >= 8
+end
+
+
+
+# ### RULE 4 MUST HAVE >= NON ALPHANUMERIC CHARACTERS
+# ##################################
+# #   Takes two strings and turns them into one string
+# #
+# #   first: string with the first name
+# #   last: string with last name
+# #
+# #   returns one string
+# #
+# #   example:
+# #   names_to_string("Davida", "Gaffney")
+# # => DavidaGaffney
+# # ---------------------------------
+# #
+
+# def not_all_alphanumeric?(password)
+#   password.length >= 8
+# end
+
+
+
+# ### RULE 5 MUST HAVE >= 1 numbers
+# ##################################
+# #   Takes two strings and turns them into one string
+# #
+# #   first: string with the first name
+# #   last: string with last name
+# #
+# #   returns one string
+# #
+# #   example:
+# #   names_to_string("Davida", "Gaffney")
+# # => DavidaGaffney
+# # ---------------------------------
+# #
+
+# def at_least_one_number(password)
+#   password.length >= 8
+# end
+
+
+
+
+# ### RULE 6 MUST NOT CONTAIN THE WORD "PASSWORD"
+# ##################################
+# #   Takes two strings and turns them into one string
+# #
+# #   first: string with the first name
+# #   last: string with last name
+# #
+# #   returns one string
+# #
+# #   example:
+# #   names_to_string("Davida", "Gaffney")
+# # => DavidaGaffney
+# # ---------------------------------
+# #
+
+# def not_including_password?(password)
+#   password.length >= 8
+# end
+
+
+
+
+
+
+# ### BONUS RULE 6 MUST NOT CONTAIN THE USER'S NAME
+# ##################################
+# #   Takes two strings and turns them into one string
+# #
+# #   first: string with the first name
+# #   last: string with last name
+# #
+# #   returns one string
+# #
+# #   example:
+# #   names_to_string("Davida", "Gaffney")
+# # => DavidaGaffney
+# # ---------------------------------
+# #
+
+# def not_including_username?(password)
+#   password.length >= 8
+# end
+
+
+
+
+
+
+# ### BONUS RULE 7 MUST NOT CONTAIN SEQUENCES OF NUMBERS OR LETTERS OF 3 IN A ROW OR MORE
+# ##################################
+# #   Takes two strings and turns them into one string
+# #
+# #   first: string with the first name
+# #   last: string with last name
+# #
+# #   returns one string
+# #
+# #   example:
+# #   names_to_string("Davida", "Gaffney")
+# # => DavidaGaffney
+# # ---------------------------------
+# #
+
+# def no_sequences_three_or_more?(password)
+#   password.length >= 8
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### FINAL RULE -- MUST PASS ** ALL ** OF THE OTHER RULES
+##################################
+#   Takes two strings and turns them into one string
+#
+#   first: string with the first name
+#   last: string with last name
+#
+#   returns one string
+#
+#   example:
+#   names_to_string("Davida", "Gaffney")
+# => DavidaGaffney
+# ---------------------------------
+#
 
 def valid_password?(password)
   at_least_one_caps?(password) && 
@@ -6,13 +232,4 @@ def valid_password?(password)
   # at_least_one_lower_case?(password)
 end
 
-# Takes a string
-# Returns true or false depending on if the string has at least one capital
-# letter
-def at_least_one_caps?(password)
-  password != password.downcase
-end
 
-def at_least_eight_characters?(password)
-  password.length >= 8
-end
--- tests/password_validator_test.rb
+++ tests/password_validator_test.rb
@@ -1,8 +1,24 @@
+# For each rule, you'll probably want to break it out into a method. Make sure to test each method. By hand, consider writing out a flowchart, or mapping a process, or writing (by hand) some test cases.
+
+# 1 or more lower-case letters
+# 8 or more characters
+# 1 or more capital letters
+# 1 or more non-alphanumeric character
+# 1 or more numbers
+# Can't contain "password"
+# Bonus Rules to do if you get done with the previous rules and want to do more.
+
+# Can't contain the user's name
+# Can't contain sequences like "abc" or "jkl" or "456"
+
 require "test_helper"
 require_relative "../lib/password_validator.rb"
 
 class PasswordValidatorTest < Minitest::Test
-  # We'll try a password with all-lower-case-letters
+
+
+
+  # We'll try a password with all-upper-case-letters
   def test_rejects_all_lower_case_passwords
     result = at_least_one_caps?("1abjils&a")
     refute(result, "'1abjils&a' should be invalid because it contains no caps")
@@ -15,6 +31,22 @@ class PasswordValidatorTest < Minitest::Test
     assert(result, "'1Abjils&a' should be valid because it is mixed case")
   end
 
+
+  # We'll try a password with all-lower-case-letters
+  def test_rejects_all_lower_case_passwords
+    result = at_least_one_downcase?("1abjils&a")
+    refute(result, "'1abjils&a' should be invalid because it contains no lowercased")
+    #assert(result == false, ".same message as above")
+  end
+
+  # We try a password with a valid mix of upper and lower-case characters
+  def test_accepts_mixed_case_passwords
+    result = at_least_one_downcase?("1Abjils&a")
+    assert(result, "'1Abjils&a' should be valid because it is mixed case")
+  end
+
+
+
   # We try a password of 8 characters, expecting success
   def test_accepts_password_of_8_characters
     result = at_least_eight_characters?("1Abjils&")
@@ -26,6 +58,11 @@ class PasswordValidatorTest < Minitest::Test
     result = at_least_eight_characters?("1Abils&")
     refute(result, "'1ABils&' has 8 characters, should be valid")
   end
+
+
+
+
+
 
   def test_valid_password
     result = valid_password?("1Abils&a")
